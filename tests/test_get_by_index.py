547f6535b1215d50f24cc70d87b92195d8620257
elias <eliaschvvarz@gmail.com>
2026-09-08 17:22:38 +0300

    created get_by_index and tests

---
+++ tests/test_get_by_index.py
@@ -0,0 +1,11 @@
+from get_by_index import get_by_index
+from random import choice, randint
+import string
+
+characters = string.ascii_letters + string.digits + '@!=*'
+
+lenght = randint(0, 70)
+
+the_elements = []
+
+if get_by_index(the_elements)
