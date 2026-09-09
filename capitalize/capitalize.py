12545b17bfa33b7376545e206d570d267da73097
elias <eliaschvvarz@gmail.com>
2026-09-09 20:50:50 +0300

    added test for int inputs for "capitalize"

--- capitalize/capitalize.py
+++ capitalize/capitalize.py
@@ -1,7 +1,9 @@
 def capitalize(input_text):
+    if isinstance(input_text, int):
+        return 'it is a number!'
+
     if not input_text:
         return ''
-    return input_text.capitalize()
     head, *tail = input_text
     rest_chars = ''.join(tail)
     return f'{head.upper()}{rest_chars}'
--- tests/test_capitalize.py
+++ tests/test_capitalize.py
@@ -1,6 +1,11 @@
 from capitalize.capitalize import capitalize
+import random
 
 
+the_number = random.randint(1, 10)
+
+assert capitalize(the_number) == 'it is a number!'
 assert capitalize('') == ''
 assert capitalize('hello') == 'Hello'
+
 print('All tests passed.')
